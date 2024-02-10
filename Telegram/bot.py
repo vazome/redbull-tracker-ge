ec3902ec734e237222c26734b0ea7292eecd7925
vazome <danielvazome@outlook.com>
2024-02-11 00:43:00 +0400

    Telegram bot logic improved

--- Telegram/bot.py
+++ Telegram/bot.py
@@ -4,167 +4,160 @@ TBD
 
 import os
 import logging
-import asyncio
+import time
+import datetime
+import pytz
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CallbackQueryHandler,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+)
+import psycopg2
 
 TG_BOT_TOKEN = os.environ["TG_BOT_TOKEN"]
 user_data = {}  # a dict to dump user responses
-districts = {
-    "Mtatsminda District": [
-        "Mtatsminda",
-        "Sololaki",
-        "Vera",
-        "Kiketi",
-        "Kojori",
-        "Shindisi",
-        "Tsavkisi",
-        "Tabakhmela",
-        "Oqrokana",
-    ],
-    "Vake District": [
-        "Vake",
-        "Bagebi",
-        "Vazha Pshavela Quarters",
-        "Tskneti",
-        "Nutsubidze Plato",
-    ],
-    "Saburtalo District": [
-        "Delisi",
-        "Vedzisi",
-        "Vashlijvari",
-        "Bakhtrioni",
-        "Khiliani",
-        "Didi Dighomi",
-        "Zurgovana",
-    ],
-    "Krtsanisi District": ["Kala", "Ortachala", "Ponichala"],
-    "Isani District": [
-        "Avlabari",
-        "Navtlughi",
-        "Metromsheni",
-        "Vazisubani",
-        "Eighth Legioni",
-    ],
-    "Samgori District": [
-        "Varketili",
-        "Third Array",
-        "Orkhevi",
-        "Dampalo",
-        "Lilo",
-        "Lower Samgori",
-    ],
-    "Chughureti District": ["Chughureti", "Kukia", "Svanetisubani"],
-    "Didube District": ["Didube", "Dighomi Massive"],
-    "Nadzaladevi District": [
-        "Nadzaladevi",
-        "Sanzona",
-        "Temka",
-        "Lotkini",
-        "Old Nadzaladevi",
-    ],
-    "Gldani District": ["Gldani Massive", "Avchala", "Mukhiani", "Gldanula"],
-}
+districts = ["Dighomi Massive", "Vake"]
 
+# Making creating states in out conversation
+(
+    USER_CHOOSING_SCHEDULE,
+    USER_RECEIVES_SCHEDULED_REPLY,
+) = range(2)
+
+# Logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
-
 logger = logging.getLogger(__name__)
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Sends a message with buttons"""
-    keyboard = [
-        [InlineKeyboardButton(district, callback_data=district)]
-        for district in districts.keys()
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Hey! This is @RedBullTrackerBot, please select you district",
-    )
-    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
-
-
-async def district_chosen(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    callback_data = query.data
-    user_id = str(
-        query.from_user.id
-    )  # Convert user ID to string to use as a dictionary key
-
-    # If the "Back" button is pressed, show the district selection again
-    if "back:districts" == callback_data:
-        keyboard = [
-            [InlineKeyboardButton(district, callback_data=district)]
-            for district in districts.keys()
-        ]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        # You can choose to either edit the message or send a new one
-        await query.edit_message_text(
-            text="Please choose a district:", reply_markup=reply_markup
-        )
-        return  # End the function here after handling the "Back" action
-
-    # If a district is chosen, show the neighborhoods in that district
-    if callback_data in districts:
-        keyboard = [[InlineKeyboardButton("← Back", callback_data="back:districts")]]
-        keyboard.extend(
-            [
-                InlineKeyboardButton(
-                    neighborhood, callback_data=f"{callback_data}:{neighborhood}"
-                )
-            ]
-            for neighborhood in districts[callback_data]
-        )
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_text(
-            text=f"Please choose a neighborhood in {callback_data}:",
-            reply_markup=reply_markup,
-        )
-    else:
-        # Simply put, the callback_data received from the user must have both district and neighborhood
-        # With below section I split the string into a list the string is presented as district:neighborhood
-        # Then I unpack the list into variables for better representation in the message
-        # We save user provided data in user_data nested dictionary
-        # And wait for the script to change the chat message with await keyword
-        parts = callback_data.split(":")
-        if len(parts) == 2:
-            district, neighborhood = parts
-            user_data[user_id] = {"district": district, "neighborhood": neighborhood}
-            await query.edit_message_text(
-                text=f"You selected {neighborhood} in {district}.\n{user_data}"
-            )
-            # Here you can add your logic to handle the user's neighborhood selection
-            await save_selection_to_database(user_id, district, neighborhood)
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Displays info on how to use the bot."""
     await update.message.reply_text(
-        "Use /start begin with this bot.\nYou will be presented district selection.\nTo change your choice use /edit"
+        "Use /start begin with this bot.\nTo start over use /cancel"
     )
 
 
-async def save_selection_to_database(user_id, district, neighborhood):
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    context.user_data.clear()
+    await update.message.reply_text("Hey!\nThis is @RedBullTrackerBot")
+    return await choose_district(update, context)
+
+
+async def choose_district(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Sends a message with district buttons"""
+    keyboard_districts = [
+        [InlineKeyboardButton(district, callback_data=district)]
+        for district in districts
+    ]
+    reply_markup_districts = InlineKeyboardMarkup(keyboard_districts)
+    # Artificial responsiveness through minimal sleep timer
+    time.sleep(1)
+    await update.message.reply_text(
+        "Please select your district:",
+        reply_markup=reply_markup_districts,
+    )
+    return USER_CHOOSING_SCHEDULE
+
+
+async def choose_schedule(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+    user_response = query.data
+
+    # Store the district selection in the context user_data for later use
+    context.user_data["district"] = user_response
+
+    # Define scheduling options buttons
+    keyboard_schedule = [
+        [InlineKeyboardButton("Get prices now", callback_data="once")],
+        [InlineKeyboardButton("Receive daily prices report", callback_data="daily")],
+    ]
+    reply_markup_schedule = InlineKeyboardMarkup(keyboard_schedule)
+
+    # Edit the message to show scheduling options instead
+    await query.edit_message_text(
+        text=f"You selected {user_response}. How do you want to proceed with the pricing information?",
+        reply_markup=reply_markup_schedule,
+    )
+
+    return USER_RECEIVES_SCHEDULED_REPLY
+
+
+async def schedule_chosen(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    await query.answer()
+
+    district_option = context.user_data["district"]
+    schedule_option = query.data  # This will be either "once" or "daily"
+
+    # Here you can implement your logic based on the schedule_option
+    # For example:
+    chat_id = update.effective_chat.id
+    scheduled_time = datetime.time(
+        hour=20, minute=59, tzinfo=pytz.timezone("Asia/Tbilisi")
+    )
+    # days = (0, 1, 2, 3, 4, 5, 6)
+    if schedule_option == "once":
+        reply_user_timing = "Getting your price now"
+        context.job_queue.run_once(
+            callback_alarm,
+            1,
+            data={"chat_id": update.effective_chat.id},
+            chat_id=chat_id,
+        )
+    if schedule_option == "daily":
+        reply_user_timing = f"You will be sent with prices schedule daily at {scheduled_time.hour}:{scheduled_time.minute}"
+        context.job_queue.run_daily(
+            callback_alarm,
+            time=scheduled_time,
+            data={"chat_id": update.effective_chat.id},
+            chat_id=chat_id,
+        )
+    await context.bot.send_message(chat_id=chat_id, text=reply_user_timing)
+    await query.edit_message_text(text=f"Your scheduling option: {schedule_option}")
+    # This should end the conversation or transition to another state as needed
+
+
+async def save_selection_to_database(user_id, district):
     # Implement the database insertion logic here
     # This will involve connecting to your database and inserting the data
     pass
+
+
+async def callback_alarm(context: ContextTypes.DEFAULT_TYPE):
+    # Beep the person who called this alarm:
+    chat_id = context.job.data["chat_id"]
+    await context.bot.send_message(chat_id=chat_id, text="BEEP!")
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    chat_id = update.message.chat_id
+    await context.bot.send_message(
+        chat_id=chat_id,
+        text=f"User {update.effective_user.username} canceled the conversation.",
+    )
+    return ConversationHandler.END
 
 
 def main() -> None:
     """Run the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TG_BOT_TOKEN).build()
-
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(district_chosen))
-    application.add_handler(CommandHandler("help", help_command))
+    convo_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            USER_CHOOSING_SCHEDULE: [CallbackQueryHandler(choose_schedule)],
+            USER_RECEIVES_SCHEDULED_REPLY: [CallbackQueryHandler(schedule_chosen)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    application.add_handler(convo_handler)
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
